22345f04fdb29dc9ac2008e9da4a5407ac1efe8b
krish <krishnaacharya608@gmail.com>
2022-03-05 11:17:04 +0530

    Third Commit

--- app/src/main/java/we/are/githubpractice/MainActivity.kt
+++ app/src/main/java/we/are/githubpractice/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("Hello there this is krishna")
+        I screwed up;
     }
 }
