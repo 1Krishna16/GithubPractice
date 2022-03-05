ee3e5cd08da78ce53e6abf31e2a613016527a8af
krish <krishnaacharya608@gmail.com>
2022-03-05 10:59:18 +0530

    Hello my first commit

---
+++ app/src/main/java/we/are/githubpractice/MainActivity.kt
@@ -0,0 +1,11 @@
+package we.are.githubpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
